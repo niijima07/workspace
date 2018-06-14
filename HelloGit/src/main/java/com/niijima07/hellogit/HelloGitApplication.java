2236e2d8800f1fbcc4f79e74eebc0b9773694f22
niijima07 <niijima07@gmail.com>
2018-06-14 09:34:52 +0900

    add new file

---
+++ HelloGit/src/main/java/com/niijima07/hellogit/HelloGitApplication.java
@@ -0,0 +1,12 @@
+package com.niijima07.hellogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGitApplication.class, args);
+	}
+}
